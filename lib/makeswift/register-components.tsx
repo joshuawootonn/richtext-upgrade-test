5161936170e7f670e0daa1831653549fcf4b5c2e
Joshua Wootonn <joshuawootonn@gmail.com>
2023-06-27 17:13:56 -0500

    feat: integrate with makeswift

--- lib/makeswift/register-components.tsx
+++ lib/makeswift/register-components.tsx
@@ -1,16 +1,59 @@
-import { Style } from '@makeswift/runtime/controls'
-import { ReactRuntime } from '@makeswift/runtime/react'
+import {
+  RichText,
+  Style,
+  unstable_RichTextV2,
+} from "@makeswift/runtime/controls";
+import {
+  InlinePlugin,
+  LinkPlugin,
+  TextAlignPlugin,
+  TypographyPlugin,
+} from "@makeswift/runtime/slate";
+
+import { ReactRuntime } from "@makeswift/runtime/react";
+import { BlockPlugin } from "@makeswift/runtime/slate";
+import { ForwardedRef, ReactNode, forwardRef } from "react";
 
 // Register your components here!
 
-function HelloWorld(props: { className?: string }) {
-  return <p {...props}>Hello, world!</p>
-}
-
-ReactRuntime.registerComponent(HelloWorld, {
-  type: 'hello-world',
-  label: 'Hello, world!',
-  props: {
-    className: Style({ properties: Style.All }),
+const RichtextUpgrade = forwardRef(function HelloWorld(
+  {
+    richtext,
+    ...props
+  }: {
+    richtext?: any;
   },
-})
+  ref: ForwardedRef<HTMLDivElement>
+) {
+  return (
+    <div
+      ref={ref}
+      style={{
+        maxWidth: "700px",
+        margin: "0 auto",
+        width: "100%",
+        padding: "20px",
+      }}
+      {...props}
+    >
+      {richtext}
+    </div>
+  );
+});
+
+ReactRuntime.registerComponent(RichtextUpgrade, {
+  type: "rich-text-component",
+  label: "rich-text-component",
+  props: {
+    // richtext: RichText(),
+    richtext: unstable_RichTextV2({
+      plugins: [
+        TypographyPlugin(),
+        BlockPlugin(),
+        TextAlignPlugin(),
+        InlinePlugin(),
+        LinkPlugin(),
+      ],
+    }),
+  },
+});
